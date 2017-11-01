f8a6293ba7a95666444e2038501d3e02ead08df1
Unknown <amiculus.umut@hotmail.de>
2017-11-01 17:13:07 +0100

    01 - i don't even know

--- 01-Angle_Correlation/data/eval.py
+++ 01-Angle_Correlation/data/eval.py
@@ -7,15 +7,23 @@ angle, run, ch1, ch2, corr, rand = np.loadtxt('source/result.dat', dtype=None, u
 #fields, constants
 d_fix			= 34							# in mm
 d				= np.array([32.4, 28.5, 31.2])	# in mm
-dba				= (d[0]/d_fix)**2				# background meas. was done at 90 degrees
 coin			= np.zeros(shape=(3, 6))
 av_coin			= np.zeros(3)
 err_coin		= np.zeros(3)
 
+# method for evaluating data: http://www.physik.kit.edu/Studium/F-Praktika/Downloads/bb_gesamt_oktober_2017.pdf
+# see section 7.7.4 (p. 147)
+
+#distance correction
 for i in range(0,3):
-	d_corr		= (d[i]/d_fix)**2																	# distance correction
-	coin[i]		= ((corr[i*6:i*6+6]*d_corr - corr[-1]*dba)-(rand[i*6:i*6+6]*d_corr - rand[-1])*dba)	# real coincidences with subtracted background and corrected distance
-	av_coin[i] 	= np.mean(coin[i])
-	err_coin[i]	= np.sqrt(av_coin[i])																# assuming poisson distribution on event count
-	err_coin[i] = err_coin[i]/np.sqrt(coin[i].size) 												# see: https://pawn.physik.uni-wuerzburg.de/~reusch/fehler/wisem0102/vorlesung7.pdf
-	print(av_coin[i], '+/-', err_coin[i])
+	d_corr				= (d[i]/d_fix)**2
+	ch1[i*6:i*6+6] 		= ch1[i*6:i*6+6]*d_corr
+	ch2[i*6:i*6+6]		= ch2[i*6:i*6+6]*d_corr
+	rand[i*6:i*6+6]		= rand[i*6:i*6+6]*d_corr
+
+# real coincidences with subtracted background
+for i in range(0,3):
+	prodch		= ch1[i*6:i*6+6]*ch2[i*6:i*6+6]
+	prodchba	= ch1[-1]*ch2[-1]
+	coin[i]		= ( corr[i*6:i*6+6]/prodch - corr[-1]/prodchba ) - ( rand[i*6:i*6+6]/prodch - rand[-1]/prodchba )
+	#print(coin[i])
