ed3460bbd190ff00e64e1355028f0fa0882fc66d
Unknown <amiculus.umut@hotmail.de>
2017-11-03 17:00:30 +0100

    01 - added relative deviations and corrected order of coefficients

--- 01-Angle_Correlation/data/eval_alt.py
+++ 01-Angle_Correlation/data/eval_alt.py
@@ -11,6 +11,7 @@ coin			= np.zeros(shape=(3, 6))		#number of coincidences
 av_coin			= np.zeros(3)					#average of coincidences per angle
 err_coin		= np.zeros(3)					#errors of avg. coincidences
 a				= np.zeros(2)					#correlation function coefficients
+
 #alternative method for evaluating data
 
 for i in range(0,3):
@@ -30,15 +31,26 @@ A = av_coin[1]/av_coin[0]
 B = av_coin[2]/av_coin[0]
 
 #correlation function coefficients
-a[0] = 2*(1+B-2*A)
-a[1] = 4*A-B-3
+a[1] = 2*(1 + B - 2*A)	#a_4
+a[0] = 4*A - B - 3		#a_2
+An = B - 1				#anisotropy
 
 #error calculation
 delta_A = np.sqrt( ( 1/av_coin[0] * err_coin[1] )**2 + ( -(A/av_coin[0]) * err_coin[0] )**2 )	#gaussian error propagation for datermination coefficients
 delta_B = np.sqrt( ( 1/av_coin[0] * err_coin[2] )**2 + ( -(B/av_coin[0]) * err_coin[0] )**2 )	#gaussian error propagation for datermination coefficients
 
 delta_a		= np.zeros(2)
-delta_a[0]	= np.sqrt( ( -4*delta_A )**2 + ( 2*delta_B )**2 )	#gaussian error propagation for coefficients
-delta_a[1]	= np.sqrt( ( 4*delta_A )**2 + ( -delta_B )**2 )		#gaussian error propagation for coefficients
+delta_a[1]	= np.sqrt( ( -4*delta_A )**2 + ( 2*delta_B )**2 )	#gaussian error propagation for coefficients
+delta_a[0]	= np.sqrt( ( 4*delta_A )**2 + ( -delta_B )**2 )		#gaussian error propagation for coefficients
+delta_An	= delta_B											#gaussian error propagation for anisotropy
 
-print('a2 = %.3f +/- %.4f, a4 = %.3f +/- %.4f' %(a[0], delta_a[0], a[1], delta_a[1]))
+print('\na2 = %.3f +/- %.4f, a4 = %.3f +/- %.4f, An = %.3f +/- %.4f' %(a[0], delta_a[0], a[1], delta_a[1], An, delta_An))
+
+#relative deviations
+theo_a	= np.array([1/8, 1/24])
+theo_An	= 0.167
+
+rel_a	= np.abs(theo_a - a)/theo_a * 100
+rel_An	= np.abs(theo_An - An)/theo_An * 100
+
+print('\nrelative deviations: a2_rel = %.2f%%, a4_rel = %.2f%%, An_rel = %.2f%%\n' %(rel_a[0], rel_a[1], rel_An))
