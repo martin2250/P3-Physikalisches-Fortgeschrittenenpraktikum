b0529b9f7182997e5d7a6da7d358f100abe189e0
Martin Pittermann <martinp2250@gmail.com>
2017-11-15 20:19:29 +0100

    02 - added kafe stuff

--- 02-Neutron-Diffusion/data/relax.py
+++ 02-Neutron-Diffusion/data/relax.py
@@ -3,26 +3,44 @@ from __future__ import division, print_function
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 #distance, count without Cd, count with Cd
 d, wo, w = np.loadtxt('values.dat', unpack=True)
-woE = np.sqrt(wo)
+woE = np.sqrt(wo)	# poisson error for series without Cd shield
 wE = np.sqrt(w)
 
+
+Y = np.log(d**2 * wo)
+YE = 1/(d**2 * wo) * woE
+
 if len(sys.argv) == 1:
-	Y = np.log(d**2 * wo)
-	YE = 1/wo * woE
 	import kafe
 	from kafe.function_library import linear_2par
+
 	dataset = kafe.Dataset(data=(d, Y))
-	dataset.add_error_source('y', 'simple', YE)
+	dataset.add_error_source('y', 'simple', YE)		# poisson error for y
+
 	fit = kafe.Fit(dataset, linear_2par)
 	fit.do_fit(quiet=True)
-	print(fit.final_parameter_values[0])
-	print(fit.final_parameter_errors[0])
-	print(fit.final_parameter_values[1])
-	print(fit.final_parameter_errors[1])
+
+	slope = fit.final_parameter_values[0]
+	slopeE = fit.final_parameter_errors[0]
+	yoffset = fit.final_parameter_values[1]
+
+	print(slope, slopeE)
+
+	os.execv(__file__, ['test', str(slope), str(slopeE), str(yoffset)])
+
+else:
+	print(sys.argv)
+	slope = float(sys.argv[1])
+	slopeE = float(sys.argv[2])
+	yoffset = float(sys.argv[3])
 
 
-plt.errorbar(d, Y, yerr=YE)
-plt.show()
+	plt.errorbar(d, Y, yerr=YE)
+
+	linX = np.array([d[0], d[-1]])
+	plt.plot(linX, slope * linX + yoffset)
+	plt.show()
