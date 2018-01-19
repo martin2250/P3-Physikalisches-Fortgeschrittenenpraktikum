e7779a4baa4aed95b4b7bd1498a093e94257c4a6
Unknown <amiculus.umut@hotmail.de>
2018-01-19 22:10:52 +0100

    05 - too unskilled in latex to "figure" out something better

--- 05-Lattice-Vibration/data/datatables-1b.py
+++ 05-Lattice-Vibration/data/datatables-1b.py
@@ -6,25 +6,20 @@ N = 4
 data_path = os.path.join(os.path.dirname(__file__), 'source')
 
 tablestart = """
-\\begin{minipage}{0.5\\textwidth}
-\\centering
-\\caption{Eigenfrequencies: Biatomic Chain, Series %i}
-\\label{tab:eigenfreqs_a1_%ib}
 \\begin{tabular}{S|SS}
 \\toprule
 {excitation $n$}\t&\t{$\\omega_{n,\\text{1}}$}\t&\t{$\\omega_{n,\\text{2}}$} \\\\
 \\midrule"""
 
 tableend = """\\bottomrule
-\\end{tabular}
-\\end{minipage}"""
+\\end{tabular}"""
 
 for i in range(N):
 	filename = 'a1-b-{:0>2d}.lvm'.format(i+1)
 	path = os.path.join(data_path, filename)
 	mode, w1, w2 = np.loadtxt(path, unpack=True)
 
-	print(tablestart%(i+1, i+1))
+	print(tablestart)
 
 	for i in range(len(mode)):
 			print("%i\t&\t%0.3f\t&\t%0.3f\t\\\\"%(mode[i]+1, 2*np.pi*w1[i], 2*np.pi*w2[i]))
